f9fb064c8d78ebb386b4b8c9ecbafdf38596f2ed
Yasha <yashasidin333@yandex.ru>
2023-02-23 01:00:55 +0300

    Add firstproject_v4

---
+++ firstproject_v4/Program.cs
@@ -0,0 +1,6 @@
+﻿// Деление
+int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA / numberB);
