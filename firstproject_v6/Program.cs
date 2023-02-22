377626fe4290eafdf6fc725317d8dfc59ab10fed
Yasha <yashasidin333@yandex.ru>
2023-02-23 01:34:42 +0300

    Add firstproject_v6

---
+++ firstproject_v6/Program.cs
@@ -0,0 +1,16 @@
+﻿// гири 
+
+int a = 1;
+int b = 2;
+int c = 4;
+int d = 6;
+int e = 10;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
