24da2090a0510ab9f8420d2cc7813780fe7a3925
Yasha <yashasidin333@yandex.ru>
2023-02-23 01:14:59 +0300

    Add firstproject_v5

---
+++ firstproject_v5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Необычное приветствие
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
